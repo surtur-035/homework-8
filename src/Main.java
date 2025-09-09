784ff737eb03d5cca0591dbdfcfde73780427d6e
surtur-035 <makarik2004981@gmail.com>
2025-09-09 16:43:09 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
